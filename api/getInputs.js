d2529f21ee98df1faa90c8c9eb015f4cff9ceed3
MaeIg <larochmael@hotmail.fr>
2022-12-01 23:51:15 +0100

    feat: add fetcher

---
+++ api/getInputs.js
@@ -0,0 +1,19 @@
+
+import * as dotenv from "dotenv";
+dotenv.config();
+
+export const getInputs = async (uri) => {
+    try {
+        const data = await fetch(uri, {
+            method: 'GET',
+            headers: {
+                'Cookie': `session=${process.env.AOC_SESSION}`,
+            }
+        });
+        const inputs = await data.text();
+
+        return inputs;
+    } catch (error) {
+        console.error("Error while fetching inputs:", error);
+    }
+}
