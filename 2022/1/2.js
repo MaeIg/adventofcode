df2fce3a36636ea3fecb99d037bb13382da36895
MaeIg <larochmael@hotmail.fr>
2022-12-01 23:51:30 +0100

    feat: 2022/1

---
+++ 2022/1/2.js
@@ -0,0 +1,15 @@
+import { getInputs } from "../../api/getInputs.js";
+
+getInputs("https://adventofcode.com/2022/day/1/input").then((inputs) => {
+    // console.log("==== Inputs ====");
+    // console.log(inputs)
+    // console.log("================");
+
+    const elves = inputs.split("\n\n").slice(0, -1);
+    
+    let sumedElves = elves.map((elf) => elf.split("\n").reduce((acc, cur) => acc + parseInt(cur), 0));
+
+    sumedElves.sort((a, b) => b - a);
+
+    console.log(sumedElves[0] + sumedElves[1] + sumedElves[2]);
+});
