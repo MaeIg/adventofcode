2e767209b9f64c7195d93b699797f40cca8271a9
MaeIg <larochmael@hotmail.fr>
2022-12-02 10:37:45 +0100

    feat: 2022/3 template

---
+++ challenges/2022/3/2.js
@@ -0,0 +1,9 @@
+import { getInputs } from "../../../api/getInputs.js";
+
+getInputs("https://adventofcode.com/2022/day/3/input").then((inputs) => {
+    // console.log("==== Inputs ====");
+    // console.log(inputs)
+    // console.log("================");
+
+    const games = inputs.split("\n").slice(0, -1);
+});
