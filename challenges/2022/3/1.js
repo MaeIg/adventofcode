f00a6671c836ae2515fc22fc8b8d723413944427
MaeIg <larochmael@hotmail.fr>
2022-12-03 21:03:42 +0100

    feat: 2022/3

--- challenges/2022/3/1.js
+++ challenges/2022/3/1.js
@@ -2,7 +2,47 @@ import { getInputs } from "../../../api/getInputs.js";
 import { parseInputs } from "../../../utils/parseInputs.js";
 
 getInputs("https://adventofcode.com/2022/day/3/input").then(inputs => {
-  // console.log("==== Inputs ====");
-  // console.log(inputs)
-  // console.log("================");
+  const getRucksackCompartment = rucksack => {
+    const a = rucksack.slice(0, rucksack.length / 2);
+    const b = rucksack.slice(rucksack.length / 2);
+    return [a, b];
+  };
+
+  const getCommonCharacters = (a, b) => {
+    const commonCharacters = [];
+
+    for (let i = 0; i < a.length; i++) {
+      for (let j = 0; j < b.length; j++) {
+        if (a[i] === b[j] && !commonCharacters.includes(a[i])) {
+          commonCharacters.push(a[i]);
+        }
+      }
+    }
+
+    return commonCharacters;
+  };
+
+  const getValue = char => {
+    const isLowerCase = char === char.toLowerCase();
+    return isLowerCase ? char.charCodeAt(0) - 96 : char.charCodeAt(0) - 38;
+  };
+
+  const rucksacks = parseInputs(inputs);
+
+  const rucksacksValues = rucksacks.map(rucksack => {
+    const [a, b] = getRucksackCompartment(rucksack);
+    const commonCharacters = getCommonCharacters(a, b);
+
+    const value = commonCharacters.reduce((acc, char) => {
+      return acc + getValue(char);
+    }, 0);
+
+    return value;
+  });
+
+  const result = rucksacksValues.reduce((acc, value) => {
+    return acc + value;
+  }, 0);
+
+  console.log(result);
 });
--- challenges/2022/3/2.js
+++ challenges/2022/3/2.js
@@ -2,9 +2,35 @@ import { getInputs } from "../../../api/getInputs.js";
 import { parseInputs } from "../../../utils/parseInputs.js";
 
 getInputs("https://adventofcode.com/2022/day/3/input").then(inputs => {
-  // console.log("==== Inputs ====");
-  // console.log(inputs)
-  // console.log("================");
+  const getCommonCharacters = (a, b, c) => {
+    const commonCharacters = [];
 
-  const games = parseInputs(inputs);
+    for (let i = 0; i < a.length; i++) {
+      for (let j = 0; j < b.length; j++) {
+        for (let k = 0; k < c.length; k++) {
+          if (a[i] === b[j] && b[j] === c[k] && !commonCharacters.includes(a[i])) {
+            commonCharacters.push(a[i]);
+          }
+        }
+      }
+    }
+
+    return commonCharacters;
+  };
+
+  const getValue = char => {
+    const isLowerCase = char === char.toLowerCase();
+    return isLowerCase ? char.charCodeAt(0) - 96 : char.charCodeAt(0) - 38;
+  };
+
+  const rucksacks = parseInputs(inputs);
+
+  let result = 0;
+  for (let i = 0; i < rucksacks.length; i += 3) {
+    const commonCharacters = getCommonCharacters(rucksacks[i], rucksacks[i + 1], rucksacks[i + 2]);
+
+    result += getValue(commonCharacters[0]);
+  }
+
+  console.log(result);
 });
