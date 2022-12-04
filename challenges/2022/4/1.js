56358bec50724df3c62e912db4631d6c946dbf62
MaeIg <larochmael@hotmail.fr>
2022-12-04 20:34:56 +0100

    feat: 2022/4

--- challenges/2022/4/1.js
+++ challenges/2022/4/1.js
@@ -2,5 +2,24 @@ import { getInputs } from "../../../api/getInputs.js";
 import { parseInputs } from "../../../utils/parseInputs.js";
 
 getInputs("https://adventofcode.com/2022/day/4/input").then(inputs => {
-  const rucksacks = parseInputs(inputs);
+  const isFullyContained = (a, b) => {
+    const a1 = Number(a[0]);
+    const a2 = Number(a[1]);
+    const b1 = Number(b[0]);
+    const b2 = Number(b[1]);
+
+    return a1 >= b1 && a2 <= b2;
+  };
+
+  const isFullyContainedDoubleCheck = (a, b) => {
+    return isFullyContained(a, b) || isFullyContained(b, a);
+  };
+
+  const pairs = parseInputs(inputs).map(pair => pair.split(",").map(n => n.split("-")));
+
+  const result = pairs.reduce((acc, pair) => {
+    return isFullyContainedDoubleCheck(pair[0], pair[1]) ? acc + 1 : acc;
+  }, 0);
+
+  console.log(result);
 });
