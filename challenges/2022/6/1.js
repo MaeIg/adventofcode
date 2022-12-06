69ac714acf4f4b42211bb308d7d73b31017c87db
MaeIg <larochmael@hotmail.fr>
2022-12-06 10:02:10 +0100

    feat: 2022/6

--- challenges/2022/6/1.js
+++ challenges/2022/6/1.js
@@ -1,6 +1,18 @@
 import { getInputs } from "../../../api/getInputs.js";
-import { parseInputs } from "../../../utils/parseInputs.js";
 
 getInputs("https://adventofcode.com/2022/day/6/input").then(data => {
-  const inputs = parseInputs(data);
+  const areDifferent = arr => {
+    const [a, b, c, d] = arr;
+    return a !== b && a !== c && a !== d && b !== c && b !== d && c !== d;
+  };
+
+  let i = 3;
+  while (i < data.length) {
+    if (areDifferent(data.slice(i - 3, i + 1))) {
+      break;
+    }
+    i++;
+  }
+
+  console.log(i + 1);
 });
--- challenges/2022/6/2.js
+++ challenges/2022/6/2.js
@@ -1,6 +1,25 @@
 import { getInputs } from "../../../api/getInputs.js";
-import { parseInputs } from "../../../utils/parseInputs.js";
 
 getInputs("https://adventofcode.com/2022/day/6/input").then(data => {
-  const inputs = parseInputs(data);
+  const areDifferent = arr => {
+    const save = [];
+    for (let i = 0; i < arr.length; i++) {
+      if (save.includes(arr[i])) {
+        return false;
+      }
+      save.push(arr[i]);
+    }
+
+    return true;
+  };
+
+  let i = 13;
+  while (i < data.length) {
+    if (areDifferent(data.slice(i - 13, i + 1))) {
+      break;
+    }
+    i++;
+  }
+
+  console.log(i + 1);
 });
